28462207e7be8ec937778754e024e9b388bb2c41
borfus <mrpeteyb@gmail.com>
2018-06-06 21:02:36 -0500

    Updated comments for main.c

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ int main()
 {
     int input;
     CvCapture* webcam = cvCreateCameraCapture(0);
+	
+	// NOTE: Checks for webcam, returns error if none found.
     if (!webcam)
     {
         puts("The program couldn't detect a webcam, Please troubleshoot your webcam and run again.");
@@ -16,14 +18,16 @@ int main()
         puts("Webcam detected. Press spacebar to save the current frame or F to change the filter.");
     }
     puts("Press ESC at any time to exit the program.");
-    cvNamedWindow("Output", 0);
+    cvNamedWindow("Output", 0); // Loads main webcam output window
 
     IplImage* image;
     IplImage* imgGray;
     int filter = 0;
     while (1)
     {
-        image = cvQueryFrame(webcam);
+		// Checks for each frame from the webcam input and displays either the RGB or grayscale
+		// version based on user's decision
+        image = cvQueryFrame(webcam); 
         if (image)
         {
             imgGray = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
@@ -39,12 +43,13 @@ int main()
             }
         }
 
+		// Checks for ascii key input
         input = cvWaitKey(10);
         if (input == 27) // If user hits escape, exit program.
         {
             break;
         }
-        else if (input == 32) // hits space
+        else if (input == 32) // hits space to save frame
         {
             if (!filter)
             {
@@ -56,7 +61,7 @@ int main()
             }
             puts("Image saved.");
         }
-        else if (input == 102) // hits 'f'
+        else if (input == 102) // hits 'f' to change filter
         {
             if (!filter)
             {
